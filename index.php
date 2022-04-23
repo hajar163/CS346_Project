7a77a7d34c1184204bb54446ae9b2a8a3084d33c
Hajar aljassar <92683935+hajar163@users.noreply.github.com>
2022-04-23 16:52:03 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+$username0 = $_POST['username'];
+$password0 = $_POST['password'];
+$submit0 = $_POST['submit'];
+?>
---
+++ insert.php
@@ -0,0 +1,40 @@
+
+
+<?php
+
+include 'connect.php';
+include 'index.php';
+
+
+$servername = "localhost";
+$username = "jeje5";
+$password = "jeje5";
+$dbname = "DB1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO Users (user_name, password)
+VALUES ('$username0', '$password0')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+
+
+<?php
+
+print "Welcome Jojo";
+print "<br>";
+print "Welcome None";
+?>
