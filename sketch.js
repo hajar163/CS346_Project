772b198531f90a6b5de423f08f090c6ed810ec3a
Reema Alqahtani <95150259+Reema-Alqahtani@users.noreply.github.com>
2022-04-28 05:12:00 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,28 @@
-let unicorn;
-let uImg;
-let tImg;
-let bImg;
-let trains = [];
+let player;
+let playerImg;
+let backgroundImg;
+let ballImg;
+let balls = [];
 let soundClassifier;
 let rightaudio=new Audio("GameMusic.mp3");
 let wrongaudio=new Audio("Game Over Sound Effect.mp3");
+var interval = null;
+var playerScore = 0;
+var gameOver=document.querySelector("#gameOver");
+
 
 function preload() {
-  const options = {
-    probabilityThreshold: 0.95
-  };
+  const options = {probabilityThreshold: 0.95};
   soundClassifier = ml5.soundClassifier('SpeechCommands18w', options);
-  uImg = loadImage('unicorn.png');
-  tImg = loadImage('train.png');
-  bImg = loadImage('background.jpg');
-  
-}
+  playerImg = loadImage('player.png');
+  ballImg = loadImage('ball.png');
+  backgroundImg=loadImage("background.jpg")
 
-function mousePressed() {
-  trains.push(new Train());
 }
 
 function setup() {
-  createCanvas(1535, 753);
-  unicorn = new Unicorn();
+  createCanvas(1535, 752);
+  player = new Player();
   soundClassifier.classify(gotCommand);
 }
 
@@ -34,35 +32,44 @@ function gotCommand(error, results) {
   }
   console.log(results[0].label, results[0].confidence);
   if (results[0].label == 'up') {
-    unicorn.jump();
+    player.jumpplayer();
   }
 }
 
 function keyPressed() {
   if (key == ' ') {
-    unicorn.jump();
+    player.jumpplayer();
    
   }
 }
 
+var scoreCounter = () => {
+  playerScore++;
+  score.innerHTML = `Score <b>${playerScore}</b>`;
+}
+
+interval = setInterval(scoreCounter,200);
+
   function draw() {
     rightaudio.play();
-    if (random(1) < 0.004) {
-      trains.push(new Train());
+    if (random(1) < 0.005) {
+      balls.push(new Ball());
     }
-  
-    background(bImg);
-    for (let t of trains) {
-      t.move();
-      t.show();
-      if (unicorn.hits(t)) {
+    background(backgroundImg);
+    gameOver.style.display = "none";
+    for (let b of balls) {
+      b.moveball();
+      b.showball();
+      if (player.hits(b)) {
         rightaudio.pause();
         wrongaudio.play();
-        window.alert('game over');
+        gameOver.style.display = "block";
+        clearInterval(interval);
         noLoop();
       }
     
     }
-    unicorn.show();
-    unicorn.move();
+    player.showplayer();
+    player.moveplayer();
+    
   }
